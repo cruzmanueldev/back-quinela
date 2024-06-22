fe0ba1f3729e7f56cc6cc3e8f2d67cff54eb2c58
Manuel Cruz <cruzmanuelar@gmail.com>
2024-06-22 08:33:58 -0500

    2106 CA: Cors Backend

--- src/app.js
+++ src/app.js
@@ -7,7 +7,14 @@ const path = require('path')
 const port = 3001
 
 const app = express()
-app.use(cors())
+app.use(cors(
+    {
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+        credentials: true
+    }
+))
 
 
 app.use(function(req, res, next){
