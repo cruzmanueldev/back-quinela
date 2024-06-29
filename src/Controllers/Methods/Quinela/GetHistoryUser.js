252dffc122d1a856c27cfb31eb0bd6625ae07dbe
Manuel Cruz <cruzmanuelar@gmail.com>
2024-06-28 23:13:58 -0500

    2806 CA: Historial Quinela

---
+++ src/Controllers/Methods/Quinela/GetHistoryUser.js
@@ -0,0 +1,79 @@
+const controller = {}
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+controller.GetHistoryUser = async (req, res) => {
+
+    const {
+        req_usuid,
+    } = req.body
+
+    let data = []
+    let statusCode  = 200
+    let response    = true
+    let message     = 'Se ha obtenido el historial con exito'
+
+    try{
+
+        data = await prisma.pruprediccionusuarios.findMany({
+            where : {
+                usuid : req_usuid,
+                pruparpartidos : {
+                    parfinalizado   : true,
+                }
+            },
+            select : {
+                prugoleslocal   : true,
+                prugolesvisita  : true,
+                pruganador      : true,
+                pruparpartidos : {
+                    select : {
+                        pargoleslocal   : true,
+                        pargolesvisita  : true,
+                        parganador      : true,
+                        parlocal        : true,
+                        parvisita       : true,
+                        parlocalsel : {
+                            select : {
+                                selimagen   : true,
+                                selabreviacion : true
+                            }
+                        },
+                        parvisitasel : {
+                            select : {
+                                selimagen   : true, 
+                                selabreviacion : true
+                            }
+                        },
+                    }
+                }
+            }
+        })
+
+        data.forEach(dat => {
+            if(dat.pruparpartidos.parlocal == dat.pruparpartidos.parganador){
+                dat['parganador'] = 1
+            }else if(dat.pruparpartidos.parvisita == dat.pruparpartidos.parganador){
+                dat['parganador'] = 2
+            }else{
+                dat['parganador'] = 0
+            }
+        });
+
+    }catch(err){
+        statusCode  = 500
+        response    = false
+        message     = 'Ha ocurrido un error al obtener el historial'
+        console.log(err)
+    }finally{
+        await prisma.$disconnect()
+        res.status(statusCode)
+            .json({
+                response,
+                message,
+                data
+            })
+    }
+}
+
+module.exports = controller
--- src/routes/Quinela/Quinela.routes.js
+++ src/routes/Quinela/Quinela.routes.js
@@ -6,13 +6,14 @@ const router = express.Router()
 const ValEditQuinela = require('../../Controllers/Validations/Quinela/EditQuinela')
 const ValGetStatistics = require('../../Controllers/Validations/Quinela/GetStatistics')
 const ValCloseQuinela = require('../../Controllers/Validations/Quinela/CloseQuinela')
-
+const ValGetHistoryUser = require('../../Controllers/Validations/Quinela/GetHistoryUser')
 
 const protectedRoutes = express.Router()
 
 protectedRoutes.post('/edit-quinela', ValEditQuinela.EditQuinela)
 protectedRoutes.post('/statistics-quinela', ValGetStatistics.GetStatistics)
 protectedRoutes.post('/close-match', ValCloseQuinela.CloseQuinela)
+protectedRoutes.post('/history-user', ValGetHistoryUser.GetHistoryUser)
 
 
 router.use('/quinela', protectedRoutes)
